7566114a6c3b1974aef837a9acfc1eb06daa5f9d
sujatagiri <sujataiet@gmail.com>
2025-06-17 18:05:27 +0530

    changes

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,10 +1,11 @@
 export interface Character {
   id: number;
-  name: string;
-  image: string;
-  status: string;
-  species: string;
-  gender: string;
-  location: {name: string};
-  origin: { name: string };
+  name?: string;
+  image?: string;
+  status?: string;
+  species?: string;
+  gender?: string;
+  location?: {name: string};
+  origin?: { name: string };
+  firstSeen?: string;
 }
